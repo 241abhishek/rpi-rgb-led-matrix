514c3a73b4cae9178b5213656bdd153374f8a90d
abhishek <241abhishek@gmail.com>
2024-02-18 23:54:30 +0000

    Added an option to specify circle color by publishing a message

--- bindings/python/led_control/src/led_control/led_control/circle_display.py
+++ bindings/python/led_control/src/led_control/led_control/circle_display.py
@@ -5,6 +5,7 @@ import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Empty
 from std_msgs.msg import Bool
+from geometry_msgs.msg import Point
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../../..'))
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
@@ -23,14 +24,14 @@ class Circle():
         self.matrix = RGBMatrix(options = options)
         self.canvas = self.matrix.CreateFrameCanvas()
     
-    def display(self, x, y, r):
+    def display(self, x, y, radius, r, g, b):
         # clear the canvas for the next frame
         self.canvas.Clear()
         # draw a circle with the center at (x, y) and radius r
-        for i in range(x - r, x + r):
-            for j in range(y - r, y + r):
-                if (i - x)**2 + (j - y)**2 <= r**2:
-                    self.canvas.SetPixel(i, j, 16, 32, 240)
+        for i in range(x - radius, x + radius):
+            for j in range(y - radius, y + radius):
+                if (i - x)**2 + (j - y)**2 <= radius**2:
+                    self.canvas.SetPixel(i, j, r, g, b)
         # swap the canvas to the display the frame
         self.canvas = self.matrix.SwapOnVSync(self.canvas)
 
@@ -57,14 +58,20 @@ class LedControl(Node):
         # set the circle properties
         self.x = 32
         self.y = 32
-        self.r = 10
+        self.radius = 10
+        # initialize the color of the circle
+        self.r = 255
+        self.g = 0
+        self.b = 0
         self.flag = False
         self.status = False
 
         # create a subscriber to the light_status topic
         self.light_status_sub = self.create_subscription(Bool, 'light_status', self.light_status_callback, 10)
 
-        
+        # create a subscriber to the circle_color topic
+        self.circle_color_srv = self.create_subscription(Point, 'circle_color', self.circle_color_callback, 10)
+
     def timer_callback(self):
         # display the circle if flag is True
         # self.get_logger().info(f"Running")
@@ -80,13 +87,18 @@ class LedControl(Node):
             # log the msg data
             # self.get_logger().info(f"{msg.data=}")
             # self.flag = 
-            self.circle.display(x=self.x, y=self.y, r=self.r)
+            self.circle.display(x=self.x, y=self.y, radius=self.radius, r=self.r, g=self.g, b=self.b)
         else:
             # log the msg data
             # self.get_logger().info(f"{msg.data=}")
             # self.flag = False
             self.circle.clear()
-    
+
+    def circle_color_callback(self, msg):
+        self.r = msg.x
+        self.g = msg.y
+        self.b = msg.z
+        self.get_logger().info(f"Color changed to ({self.r}, {self.g}, {self.b})")
 
 def main(args=None):
     rclpy.init(args=args)
